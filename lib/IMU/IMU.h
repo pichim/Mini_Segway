b09e424b89732aa6bcbb3a50ce32a5993dfb91ce
pichim <michernstpet@gmail.com>
2024-10-25 16:43:41 +0200

    First commit for PlatformIO, this needs to be doublechecked, tested and platformio.ini could be optimized

---
+++ lib/IMU/IMU.h
@@ -0,0 +1,53 @@
+#pragma once
+
+#include "mbed.h"
+#include <Eigen/Dense>
+
+#include "config.h"
+
+#include "IIRFilter.h"
+#include "Mahony.h"
+#include "mpu6500_spi.h"
+
+#ifndef M_PIf
+    #define M_PIf 3.14159265358979323846f /* pi */
+#endif
+
+class IMU
+{
+public:
+    explicit IMU(PinName pin_mosi, PinName pin_miso, PinName pin_clk, PinName pin_cl);
+    virtual ~IMU() {};
+
+    class ImuData
+    {
+    public:
+        ImuData() { init(); };
+        virtual ~ImuData() {};
+
+        Eigen::Vector3f gyro, acc;
+        Eigen::Quaternionf quat;
+        Eigen::Vector3f rpy;
+        float tilt = 0.0f;
+
+        void init() {
+            gyro.setZero();
+            acc.setZero();
+            quat.setIdentity();
+            rpy.setZero();
+        };
+    };
+
+    ImuData update();
+    bool isCalibrated() const { return m_is_calibrated; };
+
+private:
+    SPI m_spi;
+    mpu6500_spi m_ImuMPU6500;
+    Mahony m_Mahony;
+    ImuData m_ImuData;
+    IIRFilter m_gyro_filter[3];
+    IIRFilter m_acc_filter[3];
+
+    bool m_is_calibrated{false};
+};
---
+++ src/main.cpp
@@ -0,0 +1,28 @@
+// /home/pichim/.platformio/packages/framework-mbed/platformio/package_deps/py3/past/builtins/misc.py
+// 45: # from imp import reload
+// 46: from importlib import reload
+// 
+// source .platformio/penv/bin/activate
+// pip install setuptools
+
+#include "mbed.h"
+
+#include "DebounceIn.h"
+#include "MiniSegway.h"
+#include "RC.h"
+
+RC rc(MINI_SEGWAY_RC_RX); // rc needs to be declared here and passed to miniSegway
+MiniSegway miniSegway(rc);
+
+int main()
+{
+    // additional reset button
+    DebounceIn additionalResetButton(MINI_SEGWAY_RESET_BUTTON, PullUp);
+    additionalResetButton.fall(&NVIC_SystemReset);
+
+    DigitalOut led1(LED1);
+    while (true) {
+        led1 = !led1; // main thread is just blinking the green led on the nucleo
+        thread_sleep_for(1000);
+    }
+}
